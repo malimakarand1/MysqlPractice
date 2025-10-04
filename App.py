373e2cf0729f8670fd481bafeb01b99f7c9a91ba
malimakarand1 <malimakarand1@gmail.com>
2025-10-04 11:44:43 +0530

    first commit

---
+++ App.py
@@ -0,0 +1,13 @@
+import mysql.connector
+mydb=mysql.connector.connect(host="localhost", user="root", passwd="Admin", database="testdb")
+print(mydb)
+mycursor=mydb.cursor()
+#1 mycursor.execute("CREATE DATABASE testdb")
+#2 mycursor.execute("show databases")
+#3 for db in mycursor:
+#3    print(db)
+
+mycursor.execute("CREATE TABLE students (name VARCHAR(255), age INTEGER(10))")
+mycursor.execute("show tables")
+for tb in mycursor:
+    print(tb)
